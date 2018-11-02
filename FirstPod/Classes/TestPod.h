00a5748f03f6c9025ed937017d6da2121ed6adb3
张小东 <zxd2@meitu.com>
2018-11-02 21:16:15 +0800

    init

---
+++ FirstPod/Classes/TestPod.h
@@ -0,0 +1,16 @@
+//
+//  TestPod.h
+//  FirstPod
+//
+//  Created by zhangxiaodong on 2018/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestPod : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FirstPod/Classes/TestPod.m
@@ -0,0 +1,12 @@
+//
+//  TestPod.m
+//  FirstPod
+//
+//  Created by zhangxiaodong on 2018/11/2.
+//
+
+#import "TestPod.h"
+
+@implementation TestPod
+
+@end
